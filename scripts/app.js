b99d07f87bc76659d70b2eac7e9f2e508f049199
Yauheni5 <yauheni5@github.com>
2022-10-19 11:43:28 +0300

    feat: add h2, answer, and button new game

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
-let playBoard = document.getElementById('cellsForGame');
+let playBoard = document.getElementById("cellsForGame");
+let buttonNewGame = document.querySelector('.button_startGame');
 let resultQuestion = [];
 let resultUserAnswer = [];
 let numberRandomX, numberRandomY, randomCoordinateAndId;
@@ -8,18 +9,24 @@ const max = 69; //ASCII   E=69   буквы-названия ячеек
 const x = 5; //X - количество строк в таблице игры
 const y = 5; //Y - количество столбцов в таблице игры
 
-
 //функция для создания поля для игры, где X - количество строк, Y-количество столбцов
+let titleBoard = document.createElement("h2");
+titleBoard.textContent = "Запоминай и повторяй";
+titleBoard.classList = "title-board";
 
 function createFieldForGames(x, y) {
+  playBoard.textContent = "";
+  playBoard.append(titleBoard);
   for (let i = 0; i < y; i++) {
     let k = 0;
-    let stringForBoard = playBoard.appendChild(document.createElement('div'));
+    let stringForBoard = playBoard.appendChild(document.createElement("div"));
     columnBoard = String.fromCharCode(65 + i);
     stringForBoard.className = `column-${columnBoard} cells`;
     for (let j = 0; j < x; j++) {
-      let stringTwoForBoard = stringForBoard.appendChild(document.createElement('div'));
-      stringBoard = String.fromCharCode(65 + k);
+      let stringTwoForBoard = stringForBoard.appendChild(
+        document.createElement("div")
+      );
+      let stringBoard = String.fromCharCode(65 + k);
       stringTwoForBoard.className = `${stringBoard}-${columnBoard} cells`;
       stringTwoForBoard.id = `${stringBoard}-${columnBoard}`;
       k++;
@@ -31,15 +38,23 @@ function createFieldForGames(x, y) {
 
 createFieldForGames(x, y);
 
+buttonNewGame.addEventListener("click", () => {
+  createFieldForGames(x, y);
+  addClassRandomElement(resultQuestion);
+});
+
 //функция для рандомного задания(нужно запомнить ячейки выделенные красным цветом).
 
 function getRandomInt(min, max) {
-  for (resultQuestion; resultQuestion.length < memoryNumber;) {
+  resultQuestion=[];
+  for (resultQuestion; resultQuestion.length < memoryNumber; ) {
     numberRandomX = Math.floor(Math.random() * (max - min + 1)) + min;
     numberRandomY = Math.floor(Math.random() * (max - min + 1)) + min;
-    randomCoordinateAndId = `${String.fromCharCode(numberRandomX)}-${String.fromCharCode(numberRandomY)}`;
+    randomCoordinateAndId = `${String.fromCharCode(
+      numberRandomX
+    )}-${String.fromCharCode(numberRandomY)}`;
     if (resultQuestion.includes(randomCoordinateAndId)) {
-      return
+      return;
     } else {
       resultQuestion.push(randomCoordinateAndId);
     }
@@ -48,14 +63,18 @@ function getRandomInt(min, max) {
 
 //добавляет и удаляет Class ячейкам (согласно рандома)
 function addClassRandomElement(resultQuestion) {
-  for (let indForClass = 0; indForClass < resultQuestion.length; indForClass++) {
-    let numberID = resultQuestion[indForClass]
-    let docForNewClass = document.getElementById(numberID)
+  for (
+    let indexForClass = 0;
+    indexForClass < resultQuestion.length;
+    indexForClass++
+  ) {
+    let numberID = resultQuestion[indexForClass];
+    let docForNewClass = document.getElementById(numberID);
     setTimeout(() => {
-      docForNewClass.classList.add('active_cells');
+      docForNewClass.classList.add("active_cells");
     }, 2000);
     setTimeout(() => {
-      docForNewClass.classList.remove('active_cells');
+      docForNewClass.classList.remove("active_cells");
     }, 3000);
   }
 }
@@ -65,37 +84,42 @@ addClassRandomElement(resultQuestion);
 //функция для обработки Клика по ячейкам от юзера и сравнение с заданием.
 //(МОЖНО ВЫБРАТЬ ручной режим(для этого закомментить getRandomInt))
 function functionForClickMemory(stringTwoForBoard) {
+  resultUserAnswer = [];
   stringTwoForBoard.onclick = function () {
     //ручной режим выбора ячеек для загадки
     if (resultQuestion.length < memoryNumber) {
-      resultQuestion.push(this.id)
-      this.classList.add('active_cells')
+      resultQuestion.push(this.id);
+      this.classList.add("active_cells");
       setTimeout(() => {
-        this.classList.remove('active_cells');
+        this.classList.remove("active_cells");
       }, 3000);
     } else if (resultUserAnswer.length < memoryNumber) {
-      resultUserAnswer.push(this.id)
-      this.classList.add('active_cells_user')
-      console.log(resultQuestion)
-      console.log(resultUserAnswer)
+      resultUserAnswer.push(this.id);
+      this.classList.add("active_cells_user");
+      console.log(resultQuestion);
+      console.log(resultUserAnswer);
     }
 
     function equalArrays(resultQuestion, resultUserAnswer) {
-      resultQuestion = resultQuestion.sort()
-      resultUserAnswer = resultUserAnswer.sort()
+      resultQuestion = resultQuestion.sort();
+      resultUserAnswer = resultUserAnswer.sort();
       if (resultQuestion.length === resultUserAnswer.length) {
         //проверка ответа пользователя на соответствие с загаданными ячейками
+        let answerResult = document.createElement("p");
+        answerResult.classList = 'text-answer';
         for (let i = 0; i < resultQuestion.length; i++) {
           if (resultQuestion[i] !== resultUserAnswer[i]) {
-            return alert('Получится в следующий раз')
+            answerResult.textContent = "Получится в следующий раз";
+            return playBoard.append(answerResult);
           } else {
             if (i === resultQuestion.length - 1) {
-              alert('Молодец')
+              answerResult.textContent = "Молодец!";
+              return playBoard.append(answerResult);
             }
           }
         }
       }
     }
-    equalArrays(resultQuestion, resultUserAnswer)
-  }
+    equalArrays(resultQuestion, resultUserAnswer);
+  };
 }
